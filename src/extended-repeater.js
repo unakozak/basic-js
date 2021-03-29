a1452be7a0d28b54a5caa028ab6784b8dbc1b16a
Uliana Kozak <una.kozak@mail.ru>
2021-03-30 00:51:40 +0300

    feat: add solved problem extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let st = `${str}`,
+    obj = Object.assign({}, options),
+    arr = [];
+  let add = "";
+  obj.addition = `${obj.addition}`;
+
+    if (obj.separator === undefined) obj.separator = "+";
+    else obj.separator = `${obj.separator}`;
+
+    if (obj.additionSeparator === undefined) obj.additionSeparator = "|";
+    else obj.additionSeparator = `${obj.additionSeparator}`;
+
+  if (obj.additionRepeatTimes !== undefined && obj.addition !== undefined) {
+    arr[obj.additionRepeatTimes - 1] = "";
+    arr.fill(obj.addition);
+    add = arr.join(obj.additionSeparator);
+    arr = [];
+  } else if (obj.addition !== undefined) add = obj.addition;
+  else add = "";
+
+  if (add === "undefined") add = "";
+
+  st = st.concat(add);
+
+  if (obj.repeatTimes !== undefined) {
+    arr[obj.repeatTimes - 1] = "";
+    arr.fill(st);
+    st = arr.join(obj.separator);
+  }
+  return st;
 };
-  
