c2dfacfe7d521f938f23835bcd30f7e2f192053d
Uliana Kozak <una.kozak@mail.ru>
2021-03-30 00:54:24 +0300

    feat: add solved problem transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] !== "--discard-next" &&
+      arr[i] !== "--discard-prev" &&
+      arr[i] !== "--double-prev" &&
+      arr[i] !== "--double-next"
+    ) {
+      newArray.push(arr[i]);
+      continue;
+    } else if (arr[i] === "--discard-next" && arr[i + 1] !== undefined)
+      if (arr[i + 2] === "--discard-prev" || arr[i + 2] === "--double-prev")
+        i += 2;
+      else i++;
+    else if (arr[i] === "--discard-prev" && arr[i - 1] !== undefined)
+      newArray.pop();
+    else if (arr[i] === "--double-next" && arr[i + 1] !== undefined)
+      newArray.push(arr[i + 1]);
+    else if (arr[i] === "--double-prev" && arr[i - 1] !== undefined)
+      newArray.push(arr[i - 1]);
+  }
+  return newArray;
 };
